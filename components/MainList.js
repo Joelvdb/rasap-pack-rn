d79526f03ebcc4b7e4102478c6a5fff1cd048a3e
Joelvdb <joelvdb2@gmail.com>
2020-12-24 11:42:04 +0200

    almost finish

--- components/MainList.js
+++ components/MainList.js
@@ -1,6 +1,6 @@
 import { AccordionList } from "accordion-collapse-react-native";
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Linking } from 'react-native';
 import {WeaponList, FirstAidList, SoulList,OpenTalkList} from './SeconderyLists';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -9,6 +9,18 @@ import { Ionicons } from '@expo/vector-icons';
 class MainList extends React.Component {
   state = {
     list: [
+      {
+        id: 0,
+        title: 'לו״ז שבועי',
+        icon:'list',
+        body: 'React native Accordion/Collapse component, very good to use in toggles & show/hide content'
+      },
+      {
+        id: 4,
+        title: 'סימולציות',
+        icon:'walk',
+        body: 'React native Accordion/Collapse component, very good to use in toggles & show/hide content'
+      },
       {
         id: 1,
         title: 'תפיסת תפקיד הרס״פ',
@@ -34,9 +46,58 @@ class MainList extends React.Component {
 
 
 
+
     ],
   }
   _head(item) {
+    if(item.id==0){
+      return (
+        <TouchableOpacity style={{
+            flexDirection:'row-reverse',
+            flex:1,
+          margin:10,
+          borderRadius: 10,
+          backgroundColor: '#ede0d4',
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+  
+          elevation: 5,
+        }}
+        onPress={()=>Linking.openURL('https://drive.google.com/file/d/1GR97aC3rBcQ2ghLsRLr2S-3nN1N0qPVr/view?usp=sharing')}>
+            <Ionicons name={item.icon} size={32} color="green" style={{marginTop:5}} /> 
+          <Text style={{ padding: 10, fontWeight: '400',fontSize:20 }}>{item.title}</Text>
+        </TouchableOpacity>
+      );
+    }
+    if(item.id==4){
+      return (
+        <TouchableOpacity style={{
+            flexDirection:'row-reverse',
+            flex:1,
+          margin:10,
+          borderRadius: 10,
+          backgroundColor: '#ede0d4',
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+  
+          elevation: 5,
+        }}
+        onPress={()=>Linking.openURL('https://drive.google.com/file/d/1EUdj5Fn9YzJLV28b4I1t6p72hHCcfrrd/view?usp=sharing')}>
+            <Ionicons name={item.icon} size={32} color="green" style={{marginTop:5}} /> 
+          <Text style={{ padding: 10, fontWeight: '400',fontSize:20 }}>{item.title}</Text>
+        </TouchableOpacity>
+      );
+    }
     return (
       <View style={{
           flexDirection:'row-reverse',
@@ -54,7 +115,7 @@ class MainList extends React.Component {
 
         elevation: 5,
       }}>
-          <Ionicons name={item.icon} size={32} color="green" /> 
+          <Ionicons name={item.icon} size={32} color="green" style={{marginTop:5}}/> 
         <Text style={{ padding: 10, fontWeight: '400',fontSize:20 }}>{item.title}</Text>
       </View>
     );
--- components/SeconderyLists.js
+++ components/SeconderyLists.js
@@ -1,6 +1,6 @@
 import {AccordionList} from "accordion-collapse-react-native";
 import React from 'react';
-import { Button,FlatList,Linking, StyleSheet, Text, View } from 'react-native';
+import { Button,FlatList,Linking, StyleSheet, Text, View,TouchableOpacity } from 'react-native';
 
 
 class WeaponList extends React.Component{
@@ -11,25 +11,25 @@ class WeaponList extends React.Component{
                 {
                   id:1,
                   title: 'מטלת תפיסת תפקיד הרס״פ',
-                  link:9
+                  links:'https://drive.google.com/file/d/1HDi84QmL40px9qgAXPjB6O5BJQOxRM4J/view?usp=sharing'
                 },
         
                 {
                   id:2,
                   title: 'תפיסת תפקיד הרס״פ',
-                  links:9
+                  links:'https://drive.google.com/file/d/1ofHnG9vDPcIB8bwH1rqsTl4f1RDxewNk/view?usp=sharing'
                 },
         
                 {
                     id:3,
                     title: 'מקורות הכח של הרס״פ',
-                    links: 9
+                    links: 'https://drive.google.com/file/d/12JDDtrOLX8p2suPLvUuTvSJ_Am4Vu4tK/view?usp=sharing'
                   },
         
                   {
                     id:4,
                     title: 'שיחת פתיחה רס״פ',
-                    links:9
+                    links:'https://drive.google.com/file/d/1sfpUl_FRY7SAHfEh49ueoTjsOQrfWS89/view?usp=sharing'
                   },
                     
                 ],
@@ -38,9 +38,11 @@ class WeaponList extends React.Component{
 
       _head(item){
         return(
-            <View style={styles.head}>
+          <TouchableOpacity                 
+          onPress={()=>Linking.openURL(item.links)}
+           style={styles.head} >
               <Text>{item.title}</Text>
-              </View>
+              </TouchableOpacity>
         );
     }
     
@@ -77,32 +79,32 @@ class FirstAidList extends React.Component{
             {
               id:1,
               title: 'ממשקי עבודה בפורום מוביל',
-              links:9
+              links:'https://drive.google.com/file/d/1Ncg-ipClAwj02bu79okxUvrZ_7_iPBoC/view'
 
             },
     
             {
               id:2,
               title: 'ממשקי עבודה סמ״פ - רס״פ',
-              links:9
+              links:'https://drive.google.com/file/d/1JqNfKlm0FwKwK4mYKziHbVhM8x3Hr7Og/view?usp=sharing'
 
             },
     
             {
                 id:3,
                 title: 'ממשקי עבודה עם סמלים',
-                links: 9
+                links: 'https://drive.google.com/file/d/1spA1Ru6YTGjr5a-n3i6_lB_h9ZdWuhjm/view?usp=sharing'
               },
 
               {
                 id:4,
                 title: 'בניית תדריך וניהול ישיבת פרל״ג',
-                links: 9
+                links: 'https://drive.google.com/file/d/1BOwHJnvmpwz5s9Y1kF7KvlUqQUB222lo/view?usp=sharing'
               },
               {
                 id:5,
                 title: 'תפיסת חניכת סגל',
-                links: 9
+                links: 'https://drive.google.com/file/d/10lhz6Ezb6o3mPfmyAN1tL3G_QnQimM5X/view?usp=sharing'
               },
             ],
       }
@@ -110,9 +112,11 @@ class FirstAidList extends React.Component{
 
   _head(item){
     return(
-        <View style={styles.head}>
+      <TouchableOpacity                 
+      onPress={()=>Linking.openURL(item.links)}
+       style={styles.head} >
           <Text>{item.title}</Text>
-          </View>
+          </TouchableOpacity>
     );
 }
 
@@ -143,37 +147,37 @@ class SoulList extends React.Component{
             {
               id:1,
               title: 'מודל תסחים, מסדרי הנפה והשבעות',
-              links:9
+              links:'https://drive.google.com/file/d/1D4yRKVy3MMtZob07hWZDXxQuw__dhI7y/view?usp=sharing'
             },
     
             {
               id:2,
               title: 'דמ״שים, משפטים וכתיבת טפסי תלונה',
-              links: 9
+              links: 'https://drive.google.com/file/d/1sqKwEXKWRUyZmhtw8tjHd0rorrTWTN4_/view?usp=sharing'
 
             },
     
             {
                 id:3,
                 title: 'ניהול מלאי פלוגתי',
-                links: 9
+                links: 'https://drive.google.com/file/d/1WV6ke-S9R36JwoB92nyzoOYED_L9W-dX/view?usp=sharing'
 
               },
 
               {
                 id:4,
                 title: 'היערכות הרס״פ למשימות במחזור ',
-                links: 9
+                links: 'https://drive.google.com/file/d/14TZsKOxunyZLqzqfzr1Oywf9gKg4hVpc/view?usp=sharing'
               },
               {
                 id:5,
                 title: 'אירגון וניהול הלוגיסטיקה בפלוגה',
-                links: 9
+                links: 'https://drive.google.com/file/d/16UsXIe35rz76LtjBCY0eEZ6-SphMtfXQ/view'
               },
             {
               id:6,
               title: 'המענה הלוגיסטי לחייל',
-              links: 9
+              links: 'https://drive.google.com/file/d/10UPOfKKtAgTxvpHX56F6JN80MGm4yn9k/view?usp=sharing'
 
             },
 
@@ -181,7 +185,7 @@ class SoulList extends React.Component{
           {
           id:7,
           title: 'עבודה מול גורמי בסיס',
-          links: 9
+          links: 'https://drive.google.com/file/d/13UTbh36RHUEHq7xPEyk2qR9MJB0I0Ws9/view?usp=sharing'
         },
 
             
@@ -192,9 +196,11 @@ class SoulList extends React.Component{
 
   _head(item){
     return(
-        <View style={styles.head}>
+      <TouchableOpacity                 
+      onPress={()=>Linking.openURL(item.links)}
+       style={styles.head} >
           <Text>{item.title}</Text>
-          </View>
+          </TouchableOpacity>
     );
 }
 
@@ -253,15 +259,22 @@ class OpenTalkList extends React.Component{
 
   _head(item){
     return(
-        <View style={styles.head}>
+      <TouchableOpacity                 
+      onPress={()=>Linking.openURL(item.link)}
+       style={styles.head} >
           <Text>{item.title}</Text>
-          </View>
+          </TouchableOpacity>
     );
 }
 
 _body(item){
     return (
         <View style={{padding:10}}>
+          <Button 
+        style={styles.head} 
+        onPress={()=>Linking.openURL(item.link)}
+        title={item.title}
+        /> 
         </View>
     );
 }
